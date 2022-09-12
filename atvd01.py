a44a3b43b903052bdd5d9da25cf12c9e29950fbf
Arthur Henrique <109195033+arthurhenrique02@users.noreply.github.com>
2022-09-12 18:18:30 -0300

    Add files via upload

---
+++ atvd01.py
@@ -0,0 +1,11 @@
+# importar as funções
+from modulos_calc import soma, subtracao
+
+# Atrubruir valores a variáveis
+n1 = 10
+
+n2 = 5
+
+# mostrar ao usuário
+print(f'soma: {soma(n1, n2)}\n'
+      f'subtração: {subtracao(n1, n2)}')
