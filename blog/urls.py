1585a5b180dfa2738542bd9702778c8cbe1e7940
arthurhenrique02 <arthurlimanascimento@hotmail.com>
2022-09-17 19:52:53 -0300

    atualizacoes

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from .views import home, novo_contato, ver_contato, update, delete
 urlpatterns = [
     path('', home, name = 'home'),
     path('novo_contato/', novo_contato, name='novo_contato'),
-    path('post/<int:pk>/', ver_contato, name='ver_contato'),
-    path('update/<int:pk>/', update, name='update'),
-    path('dele/<int:pk>/', delete, name='delete'), 
+    path('contato/<int:pk>/', ver_contato, name='ver_contato'),
+    path('atualizar/<int:pk>/', update, name='update'),
+    path('delete/<int:pk>/', delete, name='delete'), 
 ]
